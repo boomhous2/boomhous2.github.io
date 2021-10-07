a47e2b1b1652bab6962a9601eed14bd58a09a4f7
Scott Boomhour <boomhous@med.umich.edu>
2021-10-06 23:28:08 -0400

    add assignments component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AssignmentsComponent} from "./assignments/assignments.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'assignments',
+    component: AssignmentsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AssignmentsComponent } from './assignments/assignments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssignmentsComponent
   ],
   imports: [
     BrowserModule,
