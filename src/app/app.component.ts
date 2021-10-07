c93f9244d8a5dbfe218f4fe9761a82f276f66d31
Scott Boomhour <boomhous@med.umich.edu>
2021-10-07 01:01:07 -0400

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -9,13 +9,14 @@ import {HttpClient} from "@angular/common/http";
 export class AppComponent implements OnInit{
   title = 'dexter-assignments';
 
-  canvasAssignmentsUrl = "https://dexterschools.instructure.com/api/v1/courses/7218/assignment_groups?exclude_assignment_submission_types%5B%5D=wiki_page&exclude_response_fields%5B%5D=description&exclude_response_fields%5B%5D=rubric&include%5B%5D=assignments&include%5B%5D=discussion_topic&override_assignment_dates=true&per_page=50&access_token=12351~U8SRdwOSQcRQcDt9S0xZCuPeKYyAoAEbqaQduP66chRODLDNv41ZXmokghDuLJaA";
+  canvasAssignmentsUrl = "https://dexterschools.instructure.com/api/v1/courses/7218/assignment_groups?exclude_assignment_submission_types%5B%5D=wiki_page&exclude_response_fields%5B%5D=description&exclude_response_fields%5B%5D=rubric&include%5B%5D=assignments&include%5B%5D=discussion_topic&override_assignment_dates=true&per_page=50";
   assignments: any;
 
   constructor(private readonly httpClient: HttpClient) {
   }
 
   public ngOnInit(): void {
-    this.httpClient.get(this.canvasAssignmentsUrl).subscribe(assignments => this.assignments = assignments, err => console.log("there was an error reading the canvas service"));
+    const headers = new HttpHeaders({'Authorization':'Bearer 12351~U8SRdwOSQcRQcDt9S0xZCuPeKYyAoAEbqaQduP66chRODLDNv41ZXmokghDuLJaA'});
+    this.httpClient.get(this.canvasAssignmentsUrl, {headers: headers}).subscribe(assignments => this.assignments = assignments, err => console.log("there was an error reading the canvas service"));
   }
 }
